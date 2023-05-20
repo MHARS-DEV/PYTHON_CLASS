b0d60b4eb0232312ce8e02084a6a97458a0f234b
MHARS-DEV <133828507+MHARS-DEV@users.noreply.github.com>
2023-05-19 22:21:17 -0500

    Add files via upload

---
+++ S8/Vehiculo.py
@@ -0,0 +1,21 @@
+class vehiculo :
+   
+    def __init__ (self,color,marca) : 
+        self.color = color
+        self.marca = marca
+
+carrito = vehiculo("NEGRO","LAMBORGUINI")
+
+print(f'El carro es color {carrito.color} y la marca es {carrito.marca}' )
+
+f=open('guardaCaracteristicas.txt',"w")
+texto=f'Color : {carrito.color} y la marca es :{carrito.marca}'
+f.write(texto)
+f.close()
+
+f=open('guardaCaracteristicas.txt',"r+")
+f.readlines()
+f.close()
+
+
+
