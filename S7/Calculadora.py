9719ae77970a7ec074e71d41d6a3aee5404e55e8
MHARS-DEV <133828507+MHARS-DEV@users.noreply.github.com>
2023-05-19 20:09:02 -0500

    Add files via upload

--- S7/Calculadora.py
+++ S7/Calculadora.py
@@ -1,1 +1,10 @@
+import Operaciones
+
+a: int = int(input("Ingrese el primer número : "))
+b: int = int(input("Ingrese el segundo número : "))
+
+print("La suma resultante es :",Operaciones.sumar(a,b))
+print("La resta resultante es :",Operaciones.restar(a,b))
+print("La division resultante es :",Operaciones.dividir(a,b))
+print("La multiplicación resutante es :",Operaciones.multiplicar(a,b))
 
